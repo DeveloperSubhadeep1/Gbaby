00f459bd877f341b9fe18acb3a618a673e95497d
DeveloperSubhadeep1 <161279108+DeveloperSubhadeep1@users.noreply.github.com>
2024-04-26 15:48:16 +0530

    Update about.py

--- plugins/about.py
+++ plugins/about.py
@@ -1,6 +1,6 @@
 import os 
 from pyrogram import Client, filters
-token = os.environ.get('TOKEN','')
+token = os.environ.get('TOKEN','7103264571:AAHQABT_b98LJ_tklhwsTLSaPU6OibhTocE')
 botid = token.split(':')[0]
 from helper.database import botdata, find_one, total_user
 
